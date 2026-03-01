d9dc9c8d4a404a18d83c4f2cce3012a863bbd28f
Divyansh Savita <savitadivyansh323@gmail.com>
2026-03-01 22:57:32 +0530

    pdf parser applied

--- app/api/resume/upload/route.ts
+++ app/api/resume/upload/route.ts
@@ -1,10 +1,11 @@
 import { supabase } from "@/lib/supabaseClient";
 
+export const runtime = "nodejs";
+
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
     const file = formData.get("file") as File;
-
     if (!file) {
       return new Response(JSON.stringify({ error: "No file uploaded" }), { status: 400 });
     }
@@ -15,10 +16,15 @@ export async function POST(req: Request) {
 
 const fileName = `${Date.now()}-${cleanName}`;
 
-//     // convert file to arraybuffer
+// Convert file to Buffer (pdf-parse needs Buffer)
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
+    // ✅ PDF PARSE
+    const pdfParse = require("pdf-parse");
+    const pdfData = await pdfParse(buffer);
+    const extractedText: string = pdfData.text;
 
+    console.log("Extracted length:", extractedText.length);
 //     // Upload to Supabase Storage
     const {  error: uploadError } = await supabase
       .storage
@@ -32,21 +38,25 @@ const fileName = `${Date.now()}-${cleanName}`;
       return new Response(JSON.stringify({ error: uploadError.message }), { status: 500 });
     }
 
-    const { error } = await supabase
+    const { error:dbError } = await supabase
   .from("resumes")
   .insert([
     {
       original_filename: file.name,
-      extracted_text: "",
+      file_path: fileName,
+      extracted_text: extractedText,  // 🔥 use the extracted text
       score: 0
     }
   ]);
 
-if (error) {
-  return Response.json({ error: error.message }, { status: 500 });
+if (dbError) {
+  return Response.json({ error: dbError.message }, { status: 500 });
 }
-
-    return Response.json({ message: "Upload successful" });
+//just to check
+    return Response.json({ 
+      message: "Upload successful" ,
+      length: extractedText.length
+    });
 
   } catch (error: any) {
     return Response.json({ error: error.message }, { status: 500 });
